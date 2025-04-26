d09e25be4f69b9a62ad5d42e012f198be076368e
David Nodine <davidnodine@gmail.com>
2025-04-26 16:52:20 +0200

    Feature/slotGenerator

---
+++ utils/slotGenerator.js
@@ -0,0 +1,23 @@
+module.exports = function generateTimeSlots(startTime, endTime, interval) {
+  const slots = [];
+  const toMinutes = time => {
+    const [hour, minute] = time.split(':').map(Number);
+    return hour * 60 + minute;
+  };
+
+  const to24Hour = minutes => {
+    const h = Math.floor(minutes / 60);
+    const m = minutes % 60;
+    return `${String(h).padStart(2, '0')}:${String(m).padStart(2, '0')}`;
+  };
+
+  let current = toMinutes(startTime);
+  const end = toMinutes(endTime);
+
+  while (current + interval <= end) {
+    slots.push(to24Hour(current));
+    current += interval;
+  }
+
+  return slots;
+};
