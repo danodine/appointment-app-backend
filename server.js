06f88a69b59e5b23b618f8182304da2e90f3b6b4
David Nodine <davidnodine@gmail.com>
2025-04-26 16:47:00 +0200

    Feature/server

---
+++ server.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+process.on('uncaughtException', err => {
+  console.log('UNCAGTH EXCEPTION shuting down...');
+  console.log(err);
+  process.exit(1);
+});
+
+dotenv.config({ path: './config.env' });
+const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('DB connection successful!'));
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
+
+// handle global error un handled
+
+process.on('unhandledRejection', err => {
+  console.log('Unhandled Rejection shuting down...');
+  console.log(err);
+  server.close(() => {
+    process.exit(1);
+  });
+});
